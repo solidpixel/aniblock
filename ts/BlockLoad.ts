4642cca273ec1362533436896a212de8ca0485a2
Pete Harris <pete@solidpixel.org>
2019-09-25 23:02:45 +0100

    Cleanup BlockLoad
    - Remove unused imports
    - Add or fix all public typedoc

--- ts/BlockLoad.ts
+++ ts/BlockLoad.ts
@@ -2,9 +2,7 @@
  * Aniblock: Copyright (c) 2019, Pete Harris
  */
 import { Scene } from './Scene';
-import { Link } from './Link';
-import { Block, Dir, ZOrder } from './Block';
-import { Sine } from 'gsap/TweenMax';
+import { Block } from './Block';
 
 /**
  * The ABlock provides the main building block for the diagrams, representing
@@ -12,21 +10,25 @@ import { Sine } from 'gsap/TweenMax';
  */
 export class BlockLoad extends Block {
     private isMeterVisible: boolean;
-    private currentIndex: number;
 
     /**
-     * Create a new block.
+     * Create a new block with load meter.
+     *
+     * Note that new blocks are created in a hidden state; call the
+     * [[show|`show()`]] method to animate the main block appearing. The load
+     * meter itself is a separate entity which must be shown by calling
+     * [[show_load|show_load()]].
      *
      * @param scene The parent scene.
-     * @param label The label string for this block. Note that this may be
-     *              a multi-line string; encode newlines in the input string.
-     * @param x The starting X coordinate. May be a number or a string, where
-     *          the string is the name of a vertical guide.
-     * @param y The starting Y coordinate. May be a number or a string, where
-     *          the string is the name of a horizontal guide.
+     * @param label The label string for this block. This may be a multi-line
+     *              string, with lines separated by `\n` characters.
+     * @param x The starting X coordinate; any input accepted by [[Position]].
+     * @param y The starting Y coordinate; any input accepted by [[Position]].
      * @param w The starting width.
      * @param h The starting height.
-     * @param cls The DOM class (or class list) for this block.
+     * @param cls The DOM class, or class list, for this block. Supplying this
+     *            parameter is optional, but will be needed in order to apply
+     *            custom styles to subsets of the blocks in the diagram.
      */
     constructor(
         scene: Scene,
@@ -39,7 +41,6 @@ export class BlockLoad extends Block {
     ) {
         super(scene, label, x, y, w, h, cls);
         this.isMeterVisible = false;
-        this.currentIndex = 0;
     }
 
     /*
@@ -74,13 +75,13 @@ export class BlockLoad extends Block {
     }
 
     /**
-     * Animate this block appearing.
+     * Animate the load meter appearing.
      *
-     * If the block has a zero width or zero height the show animation is
-     * instant with no tweening.
+     * TODO: Add show_load animation.
      *
-     * @param startTime The start time on the animation timeline, in seconds, or null if the
-     *                  animation should be appended to the end of the timeline.
+     * @param startTime The start time on the animation timeline, in seconds,
+     *                  or `null` if the animation should be appended to the
+     *                  end of the timeline.
      *
      * @returns The start time of this animation.
      */
@@ -103,7 +104,46 @@ export class BlockLoad extends Block {
     }
 
     /**
-     * Set load ...
+     * Animate the load meter disappearing.
+     *
+     * TODO: Add hide_load animation.
+     *
+     * @param startTime The start time on the animation timeline, in seconds,
+     *                  or `null` if the animation should be appended to the
+     *                  end of the timeline.
+     *
+     * @returns The start time of this animation.
+     */
+    public hide_load(startTime: number = null): number {
+        this.isMeterVisible = false;
+
+        let tl = this.scene.tl;
+        let tlEndTime = tl.endTime();
+        startTime = startTime == null ? tlEndTime : startTime;
+
+        let time = 1;
+        let rectId = '#' + this.id + ' rect.ALoad';
+        tl.to(rectId, time, { fillOpacity: 0, strokeOpacity: 0 }, startTime);
+
+        let rectId2 = '#' + this.id + ' rect.ALoadMeter';
+        tl.to(rectId2, time, { fillOpacity: 0, strokeOpacity: 0 }, '-=1');
+        return tlEndTime;
+    }
+
+    /**
+     * Set the current parameterization for the load meter.
+     *
+     * @param load The average load.
+     * @param range The variable range vs the average load. The load meter will
+     *              animate a variable load which fluctuates randomly between
+     *              `load - range` and `load + range`.
+     * @param startTime The start time on the animation timeline, in seconds,
+     *                  or `null` if the animation should be appended to the
+     *                  end of the timeline.
+     * @param update Set to `true` in callbacks to reuse the same code to
+     *               render the fluctuations; users should never set this.
+     *
+     * @returns The start time of this animation.
      */
     public set_load(
         load: number,
@@ -124,6 +164,7 @@ export class BlockLoad extends Block {
         if (update) {
             var startTime = tl.time();
         }
+
         let rectId = '#' + this.id + ' rect.ALoadMeter';
 
         let red = Math.floor(randLoad * 255.0);
@@ -150,29 +191,18 @@ export class BlockLoad extends Block {
         return tlEndTime;
     }
 
+    /**
+     * Callback triggered when the current random fluctuation finishes.
+     *
+     * @param load The average load.
+     * @param range The variable range vs the average load. The load meter will
+     *              animate a variable load which fluctuates randomly between
+     *              `load - range` and `load + range`.
+     */
     private complete_callback(load: number, range: number) {
         if (range != 0) {
             let tl = this.scene.tl;
             this.set_load(load, range, null, true);
         }
-    }
-
-    /**
-     * Set load ...
-     */
-    public hide_load(startTime: number = null): number {
-        this.isMeterVisible = false;
-
-        let tl = this.scene.tl;
-        let tlEndTime = tl.endTime();
-        startTime = startTime == null ? tlEndTime : startTime;
-
-        let time = 1;
-        let rectId = '#' + this.id + ' rect.ALoad';
-        tl.to(rectId, time, { fillOpacity: 0, strokeOpacity: 0 }, startTime);
-
-        let rectId2 = '#' + this.id + ' rect.ALoadMeter';
-        tl.to(rectId2, time, { fillOpacity: 0, strokeOpacity: 0 }, '-=1');
-        return tlEndTime;
     }
 }
